dd2c07d4790de938e480798ad925d85e002ea0f7
Hasitha Kaushan <hkraclh@gmail.com>
2020-08-21 18:31:48 +0530

    Unauth routing added

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Auth } from 'aws-amplify';
 
@@ -7,6 +7,7 @@ import { Auth } from 'aws-amplify';
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(private _router: Router){}
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
@@ -18,10 +19,13 @@ export class AuthGuard implements CanActivate {
             console.log(user);
             if(user){
               resolve(true);
+            }else{
+              this._router.navigate(['/login']);
             }
           })
           .catch(() => {
             //this.router.navigate(['/login']);
+            this._router.navigate(['/login']);
             resolve(false);
           });
       });
